961f3d87320302e4e5e5db12c6cf20d0385d0f50
Rohan Keshav Kanchan <134606727+rohan5255@users.noreply.github.com>
2024-02-10 18:26:59 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
     <header className={`header ${mobileMenu ? "mobileView" : " "} ${show}`}>
       <ContentWrapper>
         <div className="logo" onClick={()=>navigate("/")}>
-          <img src={logo} alt="" />
+          TMDB CLONE
         </div>
         <ul className="menuItems">
           <li className="menuItem" onClick={()=>navigationHandler("movie")}>Movies</li>
